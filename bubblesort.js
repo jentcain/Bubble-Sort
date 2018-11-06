ebaf2026802985f896ce103b9771ef315d329893
jentcain <jentcain@gmail.com>
2018-11-06 11:28:36 -0500

    minor improvements

--- bubblesort.js
+++ bubblesort.js
@@ -1,20 +1,24 @@
-function bubbleSort(array){
+// The bubbleSort function takes an array argument and sorts it by implementing the Bubble Sort algorithm. It returns a sorted array containing all of the elements of the original array.
+
+function bubbleSort(array) {
   let swapped = false;
-  for (let i = 0; i < array.length - 1; i++){
-   if (array[i] > array[i + 1]){
-    swap(array, i);
-    swapped = true;
-   }
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      swap(array, i);
+      swapped = true;
+    }
   }
-  if (swapped){
-   return bubbleSort(array);
+  if (swapped) {
+    return bubbleSort(array);
   } else {
-   return array;
+    return array;
   }
 }
 
-function swap (array, index){
-  let temp = array[index];
-  array[index] = array[index + 1];
-  array[index + 1] = temp;
- }
+// The swap function takes an array argument and an index argument and swaps the position of two elements in the array, the element at index 'idx' and the element at index 'idx + 1'
+
+function swap(array, idx) {
+  let temp = array[idx];
+  array[idx] = array[idx + 1];
+  array[idx + 1] = temp;
+}
--- mergesort.js
+++ mergesort.js
@@ -1,44 +1,42 @@
-function split(wholeArray) {
-  /* your code here to define the firstHalf and secondHalf arrays */
-  let midpoint = Math.round(wholeArray.length / 2);
-  let firstHalf = wholeArray.slice(0, midpoint);
-  let secondHalf = wholeArray.slice(midpoint);
-
-  return [firstHalf, secondHalf];
-}
-
-function merge(arr1, arr2) {
-  let sortedArr = [];
-  let leftPoint = 0;
-  let rightPoint = 0;
-  while (leftPoint < arr1.length && rightPoint < arr2.length) {
-    if (arr1[leftPoint] > arr2[rightPoint]) {
-      sortedArr.push(arr2[rightPoint]);
-      rightPoint++;
-    } else {
-      sortedArr.push(arr1[leftPoint]);
-      leftPoint++;
-    }
-  }
-  if (leftPoint === arr1.length) {
-    for (let i = rightPoint; i < arr2.length; i++) {
-      sortedArr.push(arr2[i]);
-    }
-  } else {
-    for (let i = leftPoint; i < arr1.length; i++) {
-      sortedArr.push(arr1[i]);
-    }
-  }
-  return sortedArr;
-}
+// The mergeSort function takes an array argument and sorts it by implementing the Merge Sort algorithm. It returns a sorted array containing all of the elements of the original array.
 
 function mergeSort(array) {
-  if (array.length === 1){
+  if (array.length === 1) {
     return array;
-  }
-  else {
+  } else {
     const splitArr = split(array);
     return merge(mergeSort(splitArr[0]), mergeSort(splitArr[1]));
   }
+}
+
+// The split function takes a single array argument and returns an array with two array elements, each of which has a length of 1/2 the length of the original array, if the length of the original array is even. If the length of the original array is odd, the first array element has one more element than the second.
+
+function split(wholeArray) {
+  let midpoint = Math.round(wholeArray.length / 2);
+  let firstHalf = wholeArray.slice(0, midpoint);
+  let secondHalf = wholeArray.slice(midpoint);
+  return [firstHalf, secondHalf];
+}
+
+// The merge function takes two SORTED arrays as arguments and merges them into a single sorted array
+
+function merge(leftArr, rightArr) {
+  let sortedArr = [];
+  let leftIdx = 0;
+  let rightIdx = 0;
+  while (leftIdx < leftArr.length && rightIdx < rightArr.length) {
+    if (leftArr[leftIdx] > rightArr[rightIdx]) {
+      sortedArr.push(rightArr[rightIdx++]);
+    } else {
+      sortedArr.push(leftArr[leftIdx++]);
+    }
+  }
+  for (; rightIdx < rightArr.length; rightIdx++) {
+    sortedArr.push(rightArr[rightIdx]);
+  }
+  for (; leftIdx < leftArr.length; leftIdx++) {
+    sortedArr.push(leftArr[leftIdx]);
+  }
+  return sortedArr;
 }
 
